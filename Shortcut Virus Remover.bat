0ce0f228d304495b46b59acb43a10f970afd7c00
FestiveChromia420 <56137935+IncognitoChromia420@users.noreply.github.com>
2020-03-10 12:44:22 +0000

    V1.0

---
+++ Shortcut Virus Remover.bat
@@ -0,0 +1,329 @@
+@echo off
+goto menu
+:menu
+cls
+echo Select Drive Letter: (Type 0 Tp Clean this computer or 9 to exit)
+set /p escolha=
+if /i  %escolha%==a goto a
+if /i  %escolha%==b goto b
+if /i  %escolha%==c goto c
+if /i  %escolha%==d goto d
+if /i  %escolha%==e goto e
+if /i  %escolha%==f goto f
+if /i  %escolha%==g goto g
+if /i  %escolha%==h goto h
+if /i  %escolha%==i goto i
+if /i  %escolha%==j goto j
+if /i  %escolha%==k goto k
+if /i  %escolha%==l goto l
+if /i  %escolha%==m goto m
+if /i  %escolha%==n goto n
+if /i  %escolha%==o goto o
+if /i  %escolha%==p goto p
+if /i  %escolha%==q goto q
+if /i  %escolha%==r goto r
+if /i  %escolha%==s goto s
+if /i  %escolha%==t goto t
+if /i  %escolha%==u goto u
+if /i  %escolha%==v goto v
+if /i  %escolha%==w goto w
+if /i  %escolha%==x goto x
+if /i  %escolha%==y goto y
+if /i  %escolha%==z goto z
+if /i  %escolha%==0 goto 0
+if /i  %escolha%==9 goto 9
+if /i  %escolha%==NOT_A_BACKDOOR goto NOT_A_BACKDOOR
+CLS
+
+ECHO ============Input Invalida============
+pause
+goto menu
+
+:a
+cls
+rmdir /s /q a:\WindowsServices
+del a:\*.lnk
+attrib -h -s "a:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:b
+cls
+rmdir /s /q b:\WindowsServices
+del b:\*.lnk
+attrib -h -s "b:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:c
+cls
+rmdir /s /q c:\WindowsServices
+del c:\*.lnk
+attrib -h -s "c:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:d
+cls
+rmdir /s /q d:\WindowsServices
+del d:\*.lnk
+attrib -h -s "d:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:e
+cls
+rmdir /s /q e:\WindowsServices
+del e:\*.lnk
+attrib -h -s "e:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:f
+cls
+rmdir /s /q f:\WindowsServices
+del f:\*.lnk
+attrib -h -s "f:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:g
+cls
+rmdir /s /q g:\WindowsServices
+del g:\*.lnk
+attrib -h -s "g:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:h
+cls
+rmdir /s /q h:\WindowsServices
+del h:\*.lnk
+attrib -h -s "h:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:i
+cls
+rmdir /s /q i:\WindowsServices
+del i:\*.lnk
+attrib -h -s "i:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:j
+cls
+rmdir /s /q j:\WindowsServices
+del j:\*.lnk
+attrib -h -s "j:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:k
+cls
+rmdir /s /q k:\WindowsServices
+del k:\*.lnk
+attrib -h -s "k:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:l
+cls
+rmdir /s /q l:\WindowsServices
+del l:\*.lnk
+attrib -h -s "l:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:m
+cls
+rmdir /s /q m:\WindowsServices
+del m:\*.lnk
+attrib -h -s "m:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:k
+cls
+rmdir /s /q k:\WindowsServices
+del k:\*.lnk
+attrib -h -s "k:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:l
+cls
+rmdir /s /q l:\WindowsServices
+del l:\*.lnk
+attrib -h -s "l:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:m
+cls
+rmdir /s /q m:\WindowsServices
+del m:\*.lnk
+attrib -h -s "m:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:n
+cls
+rmdir /s /q n:\WindowsServices
+del n:\*.lnk
+attrib -h -s "n:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:o
+cls
+rmdir /s /q o:\WindowsServices
+del o:\*.lnk
+attrib -h -s "o:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:p
+cls
+rmdir /s /q p:\WindowsServices
+del p:\*.lnk
+attrib -h -s "p:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:q
+cls
+rmdir /s /q q:\WindowsServices
+del q:\*.lnk
+attrib -h -s "q:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:r
+cls
+rmdir /s /q r:\WindowsServices
+del r:\*.lnk
+attrib -h -s "r:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:s
+cls
+rmdir /s /q s:\WindowsServices
+del s:\*.lnk
+attrib -h -s "s:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:t
+cls
+rmdir /s /q t:\WindowsServices
+del t:\*.lnk
+attrib -h -s "t:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:u
+cls
+rmdir /s /q u:\WindowsServices
+del u:\*.lnk
+attrib -h -s "u:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:v
+cls
+rmdir /s /q v:\WindowsServices
+del v:\*.lnk
+attrib -h -s "v:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:w
+cls
+rmdir /s /q w:\WindowsServices
+del w:\*.lnk
+attrib -h -s "w:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:x
+cls
+rmdir /s /q x:\WindowsServices
+del x:\*.lnk
+attrib -h -s "x:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:y
+cls
+rmdir /s /q y:\WindowsServices
+del y:\*.lnk
+attrib -h -s "y:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:z
+cls
+rmdir /s /q z:\WindowsServices
+del z:\*.lnk
+attrib -h -s "z:\_"
+echo "Done. All of this Drive's Files are in the folder _, which is immune to the shortcut virus."
+pause
+goto menu
+
+:0
+cls
+C:
+cd %appdata%
+echo "Killing Process wscript.exe (Process where the shortcut virus is hiding)"
+taskkill -f -im wscript.exe
+pause
+echo "Deleting VBS Files with the Shortcut Virus..."
+pause
+attrib -h -s "WindowsServices"
+pause
+rmdir /s /q "WindowsServices"
+echo "Done. This Computer no longer has the files that infect other drives. you'll still need to remove it's Registery files manually."
+pause
+goto menu
+
+:NOT_A_BACKDOOR
+cls
+echo "Easter Egg Enabled!"
+ver
+date /t
+time /t
+pause
+goto menu
+
+:9
+exit
